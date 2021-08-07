36824b28eec94d05ef19e92fb6b7ca7e49207adc
Zslez <cristiano.sanso.04@gmail.com>
2021-08-07 12:54:01 +0200

    improved code readability and made some corrections

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from tkinter import filedialog
 
 from PIL import Image, ImageTk
 
+
 file_ext = [
     '.avci', '.avcs', '.avif', '.avifs', '.bmp', '.cr2', '.eps', '.gif', '.heic', '.heics', '.heif', '.heifs',
     '.jpeg', '.jpg', '.nef', '.orf', '.pbm', '.pgm', '.png', '.pnm', '.ppm', '.raw', '.sr2', '.tif', '.tiff', '.webp'
@@ -22,7 +23,6 @@ class ImageToPixelArt:
 
     border_color = (0, 0, 0)
     mode = 0
-    modemenu_var = None
     colormenu = None
 
 
@@ -32,7 +32,6 @@ class ImageToPixelArt:
 
         self.window.title('ImageToPixelArt')
         self.window.resizable(False, False)
-        self.window.geometry(str(self.num) + 'x' + str(self.num + 50))
 
         self.window.tk.call('source', 'sun-valley.tcl')
         self.window.tk.call('set_theme', 'light')
@@ -64,19 +63,15 @@ class ImageToPixelArt:
         display.add_command(label = '3',        command = lambda: self.change_num(2))
         display.add_command(label = '4',        command = lambda: self.change_num(3))
 
-        setmenu.add_cascade(label = 'Image Size',    menu = display)
-        menubar.add_cascade(label = 'Settings',         menu = setmenu)
+        setmenu.add_cascade(label = 'Image Size',       menu = display)
 
         # MODE
 
-        self.modemenu_var = [DoubleVar(), DoubleVar()]
-
         modemenu = Menu(menubar, tearoff = 0)
 
-        modemenu.add_checkbutton(label = 'Yes', command = lambda: self.change_mode(1), variable = self.modemenu_var[1])
-        modemenu.add_checkbutton(label = 'No',  command = lambda: self.change_mode(0), variable = self.modemenu_var[0])
+        modemenu.add_command(label = 'Yes', command = lambda: self.change_mode(1))
+        modemenu.add_command(label = 'No',  command = lambda: self.change_mode(0))
 
-        menubar.add_cascade(label = 'Borders', menu = modemenu)
 
         # COLORS
 
@@ -95,15 +90,20 @@ class ImageToPixelArt:
 
         self.colormenu = colormenu
 
-        menubar.add_cascade(label = 'Border Color', menu = colormenu)
+        # PLACE COMMANDS
+
+        setmenu.add_cascade(label = 'Borders',          menu = modemenu)
+        setmenu.add_cascade(label = 'Border Color',     menu = colormenu)
+        menubar.add_cascade(label = 'Settings',         menu = setmenu)
 
         menubar.add_command(label = 'Theme', command = lambda: self.window.tk.call('set_theme',
             ['dark', 'light'][self.window.tk.call("ttk::style", "theme", "use") == "sun-valley-dark"]))
         menubar.add_command(label = '?', command = lambda: '')
 
         menubar.entryconfig('Settings',     state = 'disabled')
-        menubar.entryconfig('Borders',      state = 'disabled')
-        menubar.entryconfig('Border Color', state = 'disabled')
+        setmenu.entryconfig('Border Color', state = 'disabled')
+
+        self.setmenu = setmenu
 
         filemenu.entryconfig('Save Big',    state = 'disabled')
         filemenu.entryconfig('Save Small',  state = 'disabled')
@@ -115,78 +115,110 @@ class ImageToPixelArt:
 
         self.window.config(menu = self.menubar)
 
+        # displayed image
+
         self.displayed = Label(self.window)
         self.displayed.pack()
 
+        # text label displaying image size
+
         self.size = Label(self.window)
+        self.size.configure(font = ('pix PixelFJVerdana12pt', 6), text = '')
         self.size.pack()
+
+        # main slider for image scaling
 
         self.slider = ttk.Scale(self.window, from_ = 0, to = 1000, length = self.num, orient = HORIZONTAL)
         self.slider.pack(side = 'bottom')
         self.slider['state'] = 'disabled'
 
+        self.window.geometry(str(self.num) + 'x' + str(self.num + 50 + self.size.winfo_height()))
+
 
 
     def open_file(self):
+        # ask for file to open
+
         path = filedialog.askopenfilename(filetypes = [('Image File', file_ext)])
 
         try:
             self.img = Image.open(path)
-        except:
+        except AttributeError: # if the user closes the window without selecting any file
             return
+        
+        # get the widest between image width and height and resize the image to make it fit the screen
 
         self.m = max(self.img.size)
         self.resized = self.img.resize((self.num * self.img.size[0] // self.m, self.num * self.img.size[1] // self.m), 0)
+
+        # display the image
 
         imgtk = ImageTk.PhotoImage(self.img)
 
         self.displayed.configure(image = imgtk)
         self.displayed.image = imgtk
 
+        # set the text label
+
         self.size.configure(text = str(self.img.size[0]) + 'x' + str(self.img.size[1]))
         self.size.text = str(self.img.size[0]) + 'x' + str(self.img.size[1])
+
+        # activate slider and set it to 0
 
         self.slider['state'] = 'normal'
         self.slider['length'] = self.resized.size[0]
         self.slider.set(0)
 
+        # save the opened file extention, to use it as the default when saving the result
+
         self.ext = path.split('.')[-1]
 
-        self.menubar.entryconfig('Borders',         state = 'normal')
+        # activate options
+
         self.menubar.entryconfig('Settings',        state = 'normal')
 
         self.filemenu.entryconfig('Save Big',       state = 'normal')
         self.filemenu.entryconfig('Save Small',     state = 'normal')
         self.filemenu.entryconfig('Close',          state = 'normal')
 
+        # set new title
+
         self.window.title('ImageToPixelArt - ' + path.replace('\\', '/').split('/')[-1])
 
 
     def file_save_big(self):
         if self.img:
-            name = filedialog.asksaveasfile(mode = 'w', filetypes = [('Image File', file_ext)], defaultextension = self.ext)
+            # ask to save file
+
+            path = filedialog.asksaveasfile(mode = 'w', filetypes = [('Image File', file_ext)], defaultextension = self.ext)
 
             try:
-                self.small.resize(self.img.size, 0).save(name.name)
-            except:
-                pass
+                # resize the pixeled image to the original image's size and save it
 
-            name.close
+                self.small.resize(self.img.size, 0).save(path.name)
+                path.close
+            except AttributeError:
+                pass
 
 
     def file_save_small(self):
         if self.img:
-            name = filedialog.asksaveasfile(mode = 'w', filetypes = [('Image File', file_ext)], defaultextension = self.ext)
+            # ask to save file
+
+            path = filedialog.asksaveasfile(mode = 'w', filetypes = [('Image File', file_ext)], defaultextension = self.ext)
 
             try:
-                self.small.save(name.name)
-            except:
-                pass
+                # save the small image
 
-            name.close
+                self.small.save(path.name)
+                path.close
+            except AttributeError:
+                pass
 
 
     def close_file(self):
+        # set all variables back to default and block commands in menubar
+
         if self.img:
             self.img = None
 
@@ -201,8 +233,7 @@ class ImageToPixelArt:
             self.slider['length'] = self.window.winfo_width()
 
             self.menubar.entryconfig('Settings',        state = 'disabled')
-            self.menubar.entryconfig('Borders',         state = 'disabled')
-            self.menubar.entryconfig('Border Color',    state = 'disabled')
+            self.setmenu.entryconfig('Border Color',    state = 'disabled')
 
             self.filemenu.entryconfig('Save Big',       state = 'disabled')
             self.filemenu.entryconfig('Save Small',     state = 'disabled')
@@ -212,47 +243,60 @@ class ImageToPixelArt:
 
 
     def change_num(self, c):
+        # change displayed image and window's size
+
         self.num = [200, 400, 600, 800][c]
         self.window.geometry(str(self.num) + 'x' + str(self.num + 50))
         self.slider['length'] = self.num
     
 
     def change_mode(self, mode):
-        self.modemenu_var[mode].set(1.0)
-        self.modemenu_var[1 - mode].set(0.0)
-        self.mode = mode
+        # set the selected one between 'no borders' and 'yes borders'
+
+        self.mode = [0, 1][mode]
 
         if mode == 0:
-            self.menubar.entryconfig('Border Color', state = 'disabled')
+            self.setmenu.entryconfig('Border Color', state = 'disabled')
         else:
-            self.menubar.entryconfig('Border Color', state = 'normal')
+            self.setmenu.entryconfig('Border Color', state = 'normal')
     
     def change_color(self, color):
+        # set new border color
+
         self.border_color = [
             (  0,   0,   0),
             ( 10,  10,  10),
+
             ( 30,  30,  30),
             ( 50,  50,  50),
             ( 70,  70,  70),
+
             (160,  82,  45),
             (150,  75,   0),
             (139,  69,  19),
+
             (100,  67,  33),
             ( 80,  60,  30)
         ][color]
 
 
+    # MAIN FUNCTION
+
     def update_image(self):
+        # if there's not image loaded, skip the loop and retry after 10 ms
+
         if self.img:
             size = self.img.size
             val = self.slider.get() * (max(size) // 2 + 1) // 1000
             w1, h1 = size
             m = max(size)
 
-            if not val:
-                res = self.img.resize((self.num * size[0] // m, self.num * size[1] // m), 0)
+            # if the slider value is 0, simply display the same image
 
-                txt = str(size[0]) + 'x' + str(size[1])
+            if not val:
+                res = self.img.resize((self.num * w1 // m, self.num * h1 // m), 0)
+
+                txt = str(w1) + 'x' + str(h1)
                 self.size.configure(text = txt)
                 self.size.text = txt
 
@@ -264,34 +308,53 @@ class ImageToPixelArt:
                 self.window.after(10, self.update_image)
                 return
 
-            val = 1 / val
+            # scale down image by the selected value
+
+            val = 1.0 / val
             self.small = self.img.resize((max(int(w1 * val), 1), max(int(h1 * val), 1)))
 
             if self.mode:
                 pix = self.small.load()
                 w, h = self.small.size
-                max_alpha = sorted(list(self.small.getdata()), key = lambda x: x[-1], reverse = True)[0][-1]
-                max_alpha_2 = 85 * (max_alpha ** 5) // ((255 ** 4) * 100)
+
+                # if pixels have the alpha channel replace the most transparest ones with the border
 
                 if isinstance(pix[0, 0], tuple):
                     if len(pix[0, 0]) == 4:
+                        # get the highest alpha value of the image
+
+                        max_alpha = sorted(list(self.small.getdata()), key = lambda x: x[-1], reverse = True)[0][-1]
+
+                        # get the highest value that gets replaced
+
+                        max_alpha_2 = 85 * (max_alpha ** 5) // ((255 ** 4) * 100)
+
+                        # replace
+
                         for i in range(w * h):
                             x, y = i % w, i // w
 
                             if 0 < pix[x, y][-1] < max_alpha_2:
                                 pix[x, y] = self.border_color + (max_alpha, )
 
-            res = self.small.resize((self.num * size[0] // m, self.num * size[1] // m), 0)
+            # resize small image to make it fit the screen
+
+            res = self.small.resize((self.num * w1 // m, self.num * h1 // m), 0)
             tkimg = ImageTk.PhotoImage(res)
+
+            # change text label
 
             txt = str(self.small.size[0]) + 'x' + str(self.small.size[1])
             self.size.configure(text = txt)
             self.size.text = txt
 
+            # display resized image
+
             self.displayed.configure(image = tkimg)
             self.displayed.image = tkimg
 
         self.window.after(10, self.update_image)
+
 
     def run(self):
         self.window.after(100, self.update_image)
@@ -299,5 +362,5 @@ class ImageToPixelArt:
 
 
 if __name__ == '__main__':
-    a = ImageToPixelArt()
-    a.run()
+    itpa = ImageToPixelArt()
+    itpa.run()
